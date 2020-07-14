87df9c74e05088648c40d55ed60daaef97d81b8f
DanielleInkster <danielle.inkster@gmail.com>
2020-07-13 21:34:31 -0600

    removed pictures and stylings

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import {View, Text, Image, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 
 const App =()=>{
   return(
     <View style ={styles.container}>
-      <Text style={styles.text}> 
-        Hello World!
-        
-      </Text>
-      <Image source={{ uri: 'https://i.kym-cdn.com/photos/images/original/000/325/428/264.jpg' }} style={styles.img}/>
-     
+      <Text> 
+        Hello World! 
+      </Text>  
     </View>
 
   )
@@ -18,20 +15,6 @@ const App =()=>{
 const styles = StyleSheet.create({
   container:{
     flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center'
-  },
-
-  text:{
-    color: 'magenta', 
-    fontSize: 20
-
-  },
-
-  img:{
-    width: 200,
-    height: 200
-
   }
 })
 
