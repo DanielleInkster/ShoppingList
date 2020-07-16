0719d5afa39a9d3df9b7db1146b57ad3fc373042
DanielleInkster <danielle.inkster@gmail.com>
2020-07-15 21:50:28 -0600

    useState Hooks to create hardcode list

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './components/Header'
 import {View, Text, StyleSheet} from 'react-native'
+import { v4 as uuidv4 } from 'uuid'
 
 const App =()=>{
+
+  const [items, setItems] =useState([
+    { id: uuidv4(), text: 'milk' },
+    { id: uuidv4(), text: 'eggs' },
+    { id: uuidv4(), text: 'bread' },
+    { id: uuidv4(), text: 'juice' }
+  ])
+
   return(
     <View style ={styles.container}>
         <Header/>
