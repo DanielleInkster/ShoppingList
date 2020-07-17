8f939d290d41755af9d42cc92d8f2d0f0c6414b8
DanielleInkster <danielle.inkster@gmail.com>
2020-07-16 18:10:41 -0600

    Styled ListItem

--- components/ListItem.js
+++ components/ListItem.js
@@ -14,7 +14,20 @@ const ListItem = ({item}) => {
 }
 
 const styles = StyleSheet.create({
-    
+    listItem:{
+        padding: 15,
+        backgroundColor: '#f8f8f8',
+        borderBottomWidth: 1,
+        borderColor: '#eee',
+    },
+    listItemView:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    text:{
+        fontSize: 18,
+    } 
 })
 
 export default ListItem
