b93cbdf5cd98599b7743f1b0c6012438b8ed1631
DanielleInkster <danielle.inkster@gmail.com>
2020-07-18 01:22:11 -0600

    Setup AddItem component

---
+++ components/AddItem.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+Icon.loadFont()
+
+const AddItem = () => {
+    return (
+        <View>
+
+        </View> 
+    )
+}
+
+
+const styles = StyleSheet.create({
+
+})
+
+export default AddItem
