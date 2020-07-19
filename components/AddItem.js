87ba613fc77e70ba5a8aba8d470a2f2282ea30f8
DanielleInkster <danielle.inkster@gmail.com>
2020-07-19 00:56:10 -0600

    Styles for AddItem

--- components/AddItem.js
+++ components/AddItem.js
@@ -7,13 +7,36 @@ Icon.loadFont()
 const AddItem = () => {
     return (
         <View>
-
+            <TextInput placeholder='Enter Item' style = {styles.input}/>
+            <TouchableOpacity style = {styles.btn}>
+                <Text style = {styles.btnText}>
+                    <Icon name='plus' size={20}/>
+                    Add Item
+                </Text>
+            </TouchableOpacity>
         </View> 
     )
 }
 
 
 const styles = StyleSheet.create({
+    input:{
+        height: 60,
+        padding: 8,
+        fontSize: 16,
+    },
+
+    btn:{
+        backgroundColor: "#c2bad8",
+        padding: 9,
+        margin: 5,
+    },
+
+    btnText:{
+        color:"darkslateblue",
+        fontSize: 20,
+        textAlign:'center',
+    }
 
 })
 
